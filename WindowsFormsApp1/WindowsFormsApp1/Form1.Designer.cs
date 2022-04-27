d47874c4af8aeb9e1cb3b8fce3874b79c011a009
Sahar <100313500+sahardeldar@users.noreply.github.com>
2022-04-27 10:02:58 +0430

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_restart_Click = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_restart_Click
+            // 
+            this.btn_restart_Click.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_restart_Click.BackColor = System.Drawing.Color.RosyBrown;
+            this.btn_restart_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_restart_Click.ForeColor = System.Drawing.Color.Yellow;
+            this.btn_restart_Click.Location = new System.Drawing.Point(3, 3);
+            this.btn_restart_Click.Name = "btn_restart_Click";
+            this.btn_restart_Click.Size = new System.Drawing.Size(214, 38);
+            this.btn_restart_Click.TabIndex = 9;
+            this.btn_restart_Click.Text = "restart game";
+            this.btn_restart_Click.UseVisualStyleBackColor = false;
+            this.btn_restart_Click.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 51);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(209, 43);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "cumputer O";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(209, 42);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "user X";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(327, 223);
+            this.tableLayoutPanel1.TabIndex = 12;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.radioButton1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.radioButton2, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(63, 353);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(215, 97);
+            this.tableLayoutPanel2.TabIndex = 13;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.btn_restart_Click, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(61, 277);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(220, 44);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 473);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_restart_Click;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Button[,] Dynamicbutten;
+        int ply ,i,j;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Dynamicbutten = new Button[3, 3];
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    CreateDynamicButton();
+
+                   
+                }
+            }
+
+
+            //button1.Click += new System.EventHandler(this.Dynamicbutten_click);
+            //button2.Click += new System.EventHandler(this.Dynamicbutten_click);
+            //button3.Click += new System.EventHandler(this.Dynamicbutten_click);
+            //button4.Click += new System.EventHandler(this.Dynamicbutten_click);
+            //button5.Click += new System.EventHandler(this.Dynamicbutten_click);
+            //button6.Click += new System.EventHandler(this.Dynamicbutten_click);
+            //button7.Click += new System.EventHandler(this.Dynamicbutten_click);
+            //button8.Click += new System.EventHandler(this.Dynamicbutten_click);
+            //button9.Click += new System.EventHandler(this.Dynamicbutten_click);
+
+            newgame();
+        }
+        private void CreateDynamicButton()
+        {
+            Button Dynamicbutten = new Button();
+            Dynamicbutten.Click += new EventHandler(Dynamicbutten_click);
+            
+            Dynamicbutten.Anchor = (AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left | AnchorStyles.Bottom);
+            tableLayoutPanel1.Controls.Add(Dynamicbutten, i, j);
+        }
+
+
+        private void Dynamicbutten_click(object sender,EventArgs e)
+        {
+            MessageBox.Show("Dynamicbutten is clicked");
+        }
+
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button this_button = (sender as Button);
+            if (this_button.Text == "")
+            {
+                if (ply == 1)
+                {
+                    this_button.Text = "X";
+                    this_button.ForeColor = Color.DarkGreen;
+                    this_button.BackColor = Color.LightGreen;
+
+                    ply = 2;
+                }
+                else if (ply == 2)
+                {
+                    this_button.Text = "O";
+                    this_button.ForeColor = Color.Red;
+                    this_button.BackColor = Color.Pink;
+                    ply = 1;
+                }
+                chekgame();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            newgame();
+        }
+        private void newgame()
+        {
+            ply = 1;
+           
+            for (int i = 0; i < 3; i++)
+
+            {
+                for (int j = 0; j < 3; j++)
+                {
+
+                    Dynamicbutten[i, j].Text = "";
+                    Dynamicbutten[i, j].BackColor = Color.Purple;
+                }
+            }
+        }
+        private void chekgame()
+        {
+           if(Dynamicbutten[0,0].Text== "X" && Dynamicbutten[0,1].Text== "X" && Dynamicbutten[0, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                newgame();
+            }
+          else if (Dynamicbutten[1, 0].Text == "X" && Dynamicbutten[1, 1].Text == "X" && Dynamicbutten[1, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[2, 0].Text == "X" && Dynamicbutten[2, 1].Text == "X" && Dynamicbutten[2, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                newgame();
+            }
+
+            else if (Dynamicbutten[0, 0].Text == "X" && Dynamicbutten[1, 0].Text == "X" && Dynamicbutten[2,0].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[0, 1].Text == "X" && Dynamicbutten[1, 1].Text == "X" && Dynamicbutten[1, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[2, 0].Text == "X" && Dynamicbutten[2, 1].Text == "X" && Dynamicbutten[2, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                newgame();
+            }
+
+            else if (Dynamicbutten[0, 0].Text == "X" && Dynamicbutten[1, 1].Text == "X" && Dynamicbutten[2, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[0, 2].Text == "X" && Dynamicbutten[1, 1].Text == "X" && Dynamicbutten[2, 0].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[0, 0].Text == "O" && Dynamicbutten[0, 1].Text == "O" && Dynamicbutten[0, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[1, 0].Text == "O" && Dynamicbutten[1, 1].Text == "O" && Dynamicbutten[1, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[2, 0].Text == "O" && Dynamicbutten[2, 1].Text == "O" && Dynamicbutten[2, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[0, 0].Text == "O" && Dynamicbutten[1, 0].Text == "O" && Dynamicbutten[2, 0].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[0, 1].Text == "O" && Dynamicbutten[1, 1].Text == "O" && Dynamicbutten[1, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[2, 0].Text == "O" && Dynamicbutten[2, 1].Text == "O" && Dynamicbutten[2, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                newgame();
+            }
+
+            else if (Dynamicbutten[0, 0].Text == "O" && Dynamicbutten[1, 1].Text == "O" && Dynamicbutten[2, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[0, 2].Text == "O" && Dynamicbutten[1, 1].Text == "O" && Dynamicbutten[2, 0].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                newgame();
+            }
+            else if (Dynamicbutten[0, 0].Text != "" && Dynamicbutten[0, 1].Text != "" && Dynamicbutten[0, 2].Text != "" && Dynamicbutten[1, 0].Text != "" && Dynamicbutten[1, 1].Text != "" && Dynamicbutten[1, 2].Text != "" && Dynamicbutten[2, 0].Text != "" && Dynamicbutten[2, 1].Text != "" && Dynamicbutten[2, 2].Text != "")
+            {
+                MessageBox.Show("مساوی امتیازات برابر هستند");
+                newgame();
+            }
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+          //  if (radioButton2.Text="O")
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+} 
